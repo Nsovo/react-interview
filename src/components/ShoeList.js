60400630e557ada8f0e09b08a25324ce5d09ddd2
Nsovo <nmanganyi@fnb.co.za>
2017-03-22 14:00:12 +0200

    adding Shoe in shoelist for every item

--- src/components/ShoeList.js
+++ src/components/ShoeList.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Shoe from './Shoe';
 
-const ShoeList = (props) => (
-  <div>ShoeList</div>
-);
+class ShoeList extends React.Component{
+
+	render(){
+		return(
+		<div>
+			{
+				this.props.shoes.map((item) =>{
+					return (<Shoe shoe={item}
+						brand={item.brand}
+						name={item.name}
+						price={item.price}
+						key ={item.id} ></Shoe>)
+					
+				})
+			}
+		</div>
+		)
+	}
+}
 
 ShoeList.propTypes = {
   shoes: React.PropTypes.array.isRequired,
